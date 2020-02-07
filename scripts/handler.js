91a56c2362bfe4662b6468543f27a46f4670ce3e
Mykola Marenich <marenich.mykola@gmail.com>
2020-02-08 00:02:07 +0100

    handlerfix

--- scripts/handler.js
+++ scripts/handler.js
@@ -3,24 +3,11 @@ function calc() {
 
     var a = parseInt(document.querySelector("#value1").value);
     var b = parseInt(document.querySelector("#value2").value);
-    var op = document.querySelector("#operator").value;
-
+    var operation = document.querySelector("#operator").value;
+    var calculate;
   // pass user input through core logic
 
-  var calculate;
-
-    if (op == "add") {
-      calculate = a + b;
-    } 
-    else if (op == "min") {
-      calculate = a - b;
-    }
-    else if (op == "div") {
-      calculate = a / b;
-    }
-    else if (op == "mul") {
-      calculate = a * b;
-    }
+  calculate = doMath(operation, a, b)
 
   // render output to DOM for user
 
